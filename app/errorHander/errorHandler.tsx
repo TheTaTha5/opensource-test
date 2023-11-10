bc3c2ac62c3fef37b34f1da2dd22a0193f968dd4
Kamin <ta-zaza2012@hotmail.com>
2023-11-10 11:23:35 +0700

    run npm build and delete eslint error

--- app/errorHander/errorHandler.tsx
+++ app/errorHander/errorHandler.tsx
@@ -3,8 +3,8 @@
 import { useRouter } from "next/navigation";
 
 const error = ({error,reset} : {error: Error; reset:()=> void}) => {
-    const router = useRouter();
-    return <div>opppss error : {error.message} occure T^T <button onClick={router.back} className="goBackButton"> Go Back
+
+    return <div>opppss error : {error.message} occure T^T <button className="goBackButton"> Go Back
         </button></div>
 }
 
