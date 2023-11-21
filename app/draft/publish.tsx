386a0f774b88f2ca265b19b24c64e082623a0a7b
kamin <ta-zaza2012@hotmail.com>
2023-11-22 03:05:44 +0700

    make publishDraft fuction a recursion

--- app/draft/publish.tsx
+++ app/draft/publish.tsx
@@ -4,20 +4,21 @@ import { fetchingError } from "../errorHander/exceptions";
 export const publishDraft = async ({id} : {id:string})  => {
     const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-    const raw = 
-    JSON.stringify({
-      "published": true,
-    });
+
     const requestOptions = {
         method: "PATCH",
         headers: myHeaders,
-        body: raw,
+        body: JSON.stringify({
+          "published": true,
+        }),
       };
  const res = await fetch(`https://post-api.opensource-technology.com/api/posts/${id}`, requestOptions).then((result)=> {
     console.log("patch publish to true === "+ id)
     if(!result.ok) {
-      throw new fetchingError();
-    } 
+      publishDraft({id:id});
+    } else {
+      console.log("publish darft succeed " + result.status)
+    }
     return result;
  })
 
